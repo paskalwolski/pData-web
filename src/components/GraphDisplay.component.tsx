d8c37554a86fa11fd8e73654487c652eb690fdbc
Paskal W <paskal.wolski@gmail.com>
2024-10-12 21:08:31 +0200

    Added mouse tracking point to graphs

--- src/components/GraphDisplay.component.tsx
+++ src/components/GraphDisplay.component.tsx
@@ -12,7 +12,7 @@ const GraphDisplay = ({ selectedLap, ...props }) => {
 
     useEffect(() => {
         setSaniData(selectedLap.lap_data.sort((a, b) => a.distance - b.distance))
-        
+
     }, [selectedLap])
 
     useEffect(() => {
@@ -33,25 +33,33 @@ const GraphDisplay = ({ selectedLap, ...props }) => {
                 .range([0, width])
             svg.append("g")
                 .attr("transform", `translate(${margins.left}, ${gHeight - margins.bottom})`)
-                .call(d3.axisBottom(xScale))
+                .classed("axis", true)
+                .call(d3.axisBottom(xScale)
+                    .ticks(8))
 
             const domain = d3.extent(selectedLap.lap_data, data => data.speed)
-            const bufferedDomain = [domain[0] *0.95, domain[1] * 1.05]
+            const bufferedDomain = [domain[0] * 0.95, domain[1] * 1.05]
             const yScale = d3.scaleLinear()
                 // .domain([0, maxSpeed])
                 .domain(bufferedDomain)
                 .range([height, 0])
             svg.append("g")
                 .attr("transform", `translate(${margins.left}, ${margins.top})`)
+                .classed("axis", true)
                 .call(d3.axisLeft(yScale)
                     .ticks(4))
 
-            const lineGenerator = d3.line()
-                .x(d => xScale(d.distance))
-                .y(d => yScale(d.speed))
-
             const g = svg.append("g")
                 .attr("transform", `translate(${margins.left}, ${margins.top})`)
+            const focusPoint = g.append("circle")
+                .style('opacity', 0)
+                .style('fill', "none")
+                .attr('stroke', 'black')
+                .attr('r', 5)
+
+            const lineGenerator = d3.line()
+                .x(d => xScale(Number(d.distance).toFixed(0)))
+                .y(d => yScale(d.speed))
 
             g.append("path")
                 .datum(saniData)
@@ -59,6 +67,31 @@ const GraphDisplay = ({ selectedLap, ...props }) => {
                 .attr('stroke', 'steelblue')
                 .attr('stroke-width', 2)
                 .attr('fill', "none")
+            
+            const mouseover = () => {
+                focusPoint.style("opacity", 1)
+            }
+            const bisector = d3.bisector(d => d.distance).left
+            const mousemove = (e) => {
+                const x0 = xScale.invert(d3.pointer(e)[0])
+                const i = bisector(saniData, x0)
+                const selectedData = saniData[i]
+                focusPoint
+                    .attr("cx", xScale(selectedData.distance))
+                    .attr("cy", yScale(selectedData.speed))
+            }
+            const mouseout = () => {
+                focusPoint.style("opacity", 0)
+            }
+            g.append('rect')
+                .style("fill", "none")
+                .style("pointer-events", "all")
+                .attr('width', width)
+                .attr('height', height)
+                .on('mouseover', mouseover)
+                .on('mousemove', mousemove)
+                .on('mouseout', mouseout);
+            
         }
     }, [saniData])
 
