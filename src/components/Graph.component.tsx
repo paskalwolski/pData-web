6fe20796f87ca599ed8ed18bb3f5ff01df928c48
Paskal W <paskal.wolski@gmail.com>
2024-10-12 23:28:53 +0200

    Adjusted the graph to use state to highlight a specific point

--- src/components/Graph.component.tsx
+++ src/components/Graph.component.tsx
@@ -1,8 +1,11 @@
 import * as d3 from "d3";
+import { useEffect, useRef, useState } from "react";
 
 const Graph = ({ selectedLap, selectedPoint, setSelectedPoint, ...props }) => {
     const height = 200;
     const width = 500;
+    const focusRef = useRef(null);
+    const [focusCoord, setFocusCoord] = useState([0, 0]);
 
     const saniData = selectedLap.lap_data.sort(
         (a, b) => a.distance - b.distance
@@ -25,8 +28,26 @@ const Graph = ({ selectedLap, selectedPoint, setSelectedPoint, ...props }) => {
         .line()
         .x((d) => xScale(Number(Number(d.distance).toFixed(0))))
         .y((d) => yScale(d.speed));
-
     const linePath = lineGenerator(saniData);
+
+    const handleMouseEnter = () => {
+        return;
+    };
+    const handleMouseMove = (e) => {
+        const x0 = xScale.invert(d3.pointer(e)[0]);
+        const i = d3.bisector((d) => d.distance).left(saniData, x0);
+        setSelectedPoint(i);
+    };
+    const handleMouseLeave = () => {};
+
+    useEffect(() => {
+        if (!selectedPoint) return;
+        const selectedData = saniData[selectedPoint];
+        setFocusCoord([
+            xScale(selectedData.distance),
+            yScale(selectedData.speed),
+        ]);
+    }, [selectedPoint]);
 
     return (
         <div>
@@ -34,10 +55,28 @@ const Graph = ({ selectedLap, selectedPoint, setSelectedPoint, ...props }) => {
                 {/* TODO: Draw the axis? */}
                 <path
                     d={linePath}
-                    fill="none"
                     stroke="steelblue"
                     strokeWidth={2}
-                ></path>
+                    fill="none"
+                />
+                <g>
+                    <circle
+                        ref={focusRef}
+                        cx={focusCoord[0] ?? 0}
+                        cy={focusCoord[1] ?? 0}
+                        opacity={1}
+                        r={5}
+                    />
+                    <rect
+                        style={{ pointerEvents: "all" }}
+                        fill="none"
+                        height={height}
+                        width={width}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseMove={handleMouseMove}
+                        onMouseLeave={handleMouseLeave}
+                    />
+                </g>
             </svg>
         </div>
     );
