862d9b63909a0b5b86983116aa83efb32db09751
Paskal W <paskal.wolski@gmail.com>
2024-10-13 14:57:39 +0200

    Made a component from the Track Line to help avoid unnecessary rerenders

--- src/components/Track.component.tsx
+++ src/components/Track.component.tsx
@@ -1,56 +1,50 @@
 import * as d3 from "d3";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import TrackLine from "./TrackLine.component";
 
 const Track = ({ selectedLap, selectedPoint, setSelectedPoint }) => {
     const focusRef = useRef(null);
     const [focusPos, setFocusPos] = useState([0, 0]);
     const [focusVisible, setFocusVisible] = useState(false);
-    const saniData = selectedLap.lap_data.sort(
-        (a, b) => a.distance - b.distance
+    const saniData = useMemo(
+        () => selectedLap.lap_data.sort((a, b) => a.distance - b.distance),
+        [selectedLap.lap_number]
     );
-    const dataPoints = saniData.length;
     const height = 500;
     const width = 500;
 
-    const xScale = d3
-        .scaleLinear()
-        .domain(d3.extent(selectedLap.lap_data, (data) => data.pos[0]))
-        .range([0, width]);
-    const yScale = d3
-        .scaleLinear()
-        .domain(d3.extent(selectedLap.lap_data, (data) => data.pos[2]))
-        .range([0, height]);
+    const xScale = useMemo(
+        () =>
+            d3
+                .scaleLinear()
+                .domain(d3.extent(selectedLap.lap_data, (data) => data.pos[0]))
+                .range([0, width]),
+        [saniData.lap_number]
+    );
+    const yScale = useMemo(
+        () =>
+            d3
+                .scaleLinear()
+                .domain(d3.extent(selectedLap.lap_data, (data) => data.pos[2]))
+                .range([0, height]),
+        [saniData.lap_number]
+    );
 
-    const lineGenerator = d3
-        .line()
-        .x((d) => xScale(d.pos[0]))
-        .y((d) => yScale(d.pos[2]))
-        .curve(d3.curveCatmullRom);
-
-    const colorGen = (n) => {
-        let gas, brake;
-        ({ gas, brake } = n);
-        if (gas != 0 && brake != 0) {
-            return `#000000`;
-        } else if (gas) {
-            return d3.hsl(120, 1, 1 - gas * 0.5).formatHex8();
-        } else if (brake) {
-            return d3.hsl(0, 1, 1 - brake * 0.5).formatHex8();
-        } else {
-            return "#FFFFFF";
-        }
-    };
+    const setFocus = useMemo(
+        // Factory which returns a function
+        () => (i) => {
+            setSelectedPoint(i);
+            const d = saniData[i];
+            setFocusPos([xScale(d.pos[0]), yScale(d.pos[2])]);
+            setFocusVisible(true);
+        },
+        [saniData]
+    );
 
     useEffect(() => {
         if (!selectedPoint) return;
         setFocus(selectedPoint);
     }, [selectedPoint]);
-
-    const setFocus = (i) => {
-        const d = saniData[i];
-        setFocusPos([xScale(d.pos[0]), yScale(d.pos[2])]);
-        setFocusVisible(true);
-    };
 
     const showFocus = () => {
         console.log("Showing Focus");
@@ -64,25 +58,11 @@ const Track = ({ selectedLap, selectedPoint, setSelectedPoint }) => {
     return (
         <div>
             <svg width={width} height={height} style={{ margin: "10px" }}>
-                <g>
-                    {saniData.map((data, i) => {
-                        if (i == dataPoints - 1) return;
-                        const lineData = lineGenerator([data, saniData[i + 1]]);
-                        return (
-                            <path
-                                d={lineData}
-                                fill={"none"}
-                                stroke={colorGen(data)}
-                                strokeWidth={2}
-                                id={`path-${i}`}
-                                key={`path-${i}`}
-                                onMouseOver={() => {
-                                    setFocus(i);
-                                }}
-                            />
-                        );
-                    })}
-                </g>
+                {saniData && (
+                    <TrackLine
+                        {...{ data: saniData, xScale, yScale, setFocus }}
+                    />
+                )}
                 {focusVisible && (
                     <circle
                         ref={focusRef}
---
+++ src/components/TrackLine.component.tsx
@@ -0,0 +1,47 @@
+import * as d3 from "d3";
+import React from "react";
+
+const TrackLine = React.memo(({ data, xScale, yScale, setFocus }) => {
+    const lineGenerator = d3
+        .line()
+        .x((d) => xScale(d.pos[0]))
+        .y((d) => yScale(d.pos[2]))
+        .curve(d3.curveCatmullRom);
+
+    const colorGen = (n) => {
+        let gas, brake;
+        ({ gas, brake } = n);
+        if (gas != 0 && brake != 0) {
+            return `#000000`;
+        } else if (gas) {
+            return d3.hsl(120, 1, 1 - gas * 0.5).formatHex8();
+        } else if (brake) {
+            return d3.hsl(0, 1, 1 - brake * 0.5).formatHex8();
+        } else {
+            return "#FFFFFF";
+        }
+    };
+
+    return (
+        <g>
+            {data.map((d, i) => {
+                if (i == data.length - 1) return;
+                const lineData = lineGenerator([d, data[i + 1]]);
+                return (
+                    <path
+                        d={lineData}
+                        fill={"none"}
+                        stroke={colorGen(d)}
+                        strokeWidth={2}
+                        id={`path-${i}`}
+                        key={`path-${i}`}
+                        onMouseOver={() => {
+                            setFocus(i);
+                        }}
+                    />
+                );
+            })}
+        </g>
+    );
+});
+export default TrackLine;
