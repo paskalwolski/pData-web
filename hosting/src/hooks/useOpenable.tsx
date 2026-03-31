e723d7b9e657e0b5488ed417c808cb37724ccbf8
Paskal W <paskal.wolski@gmail.com>
2026-03-31 16:23:17 +0200

    [Utils] Adds openable hook

---
+++ hosting/src/hooks/useOpenable.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const useOpeneable = (initialState: boolean = false) => {
+    const [isOpen, setOpen] = useState(initialState);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const toggleOpen = setOpen((currentIsOpen) => !currentIsOpen);
+
+    return [isOpen, toggleOpen, handleOpen, handleClose, setOpen] as const;
+};
+
+export { useOpeneable };
