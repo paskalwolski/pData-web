fbc0ce5a8e22d388aaf00f181291e57e0e43f938
Paskal W <paskal.wolski@gmail.com>
2026-05-24 10:25:02 +0200

    [Laps] Adds column for expiry to lapTable
    - Includes default sorting by lap time

--- hosting/src/components/LapTable.component.tsx
+++ hosting/src/components/LapTable.component.tsx
@@ -8,47 +8,20 @@ import { useLapTableData } from "../hooks/useLaps";
 import type { LapData } from "../types";
 import { formatDuration } from "../helpers/formatDuration";
 import { Timestamp } from "firebase/firestore";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { navigate } from "wouter/use-browser-location";
+import { TbClockX } from "react-icons/tb";
+import { Box, useTheme } from "@mui/material";
 
 const getRowId = (row: LapData) => row.lapId;
-const columns: GridColDef<LapData>[] = [
-    { field: "lapTime", headerName: "Time", valueFormatter: formatDuration },
-    {
-        field: "trackName",
-        headerName: "Track",
-        valueGetter: (_, row) => row.sessionData.track,
-        width: 250,
-    },
-    {
-        field: "carName",
-        headerName: "Car",
-        valueGetter: (_, row) => row?.sessionData.car,
-        width: 250,
-    },
-    {
-        field: "driverName",
-        headerName: "Driver",
-        valueGetter: (_, row: LapData) => row?.sessionData.driver,
-        width: 200,
-    },
-    {
-        field: "lapTimestamp",
-        headerName: "Date",
-        valueFormatter: (value: Timestamp) => {
-            return value.toDate().toLocaleString("en-fr", {
-                dateStyle: "short",
-            });
-        },
-        width: 160,
-    },
-];
 
 interface Props {
     onLapSelect?: (lapId: string) => undefined;
 }
 
 const LapTable = ({ onLapSelect }: Props) => {
+    const { palette } = useTheme();
+
     const [pagination, setPagination] = useState<GridPaginationModel>({
         pageSize: 10,
         page: 0,
@@ -71,6 +44,65 @@ const LapTable = ({ onLapSelect }: Props) => {
         ({ row }) =>
             onLapSelect ? onLapSelect(row.lapId) : navigateToLap(row.lapId),
         [onLapSelect],
+    );
+
+    const columns: GridColDef<LapData>[] = useMemo(
+        () => [
+            {
+                field: "lapTime",
+                headerName: "Time",
+                valueFormatter: formatDuration,
+                sortingOrder: ["asc", null],
+            },
+            {
+                field: "trackName",
+                headerName: "Track",
+                valueGetter: (_, row) => row.sessionData.track,
+                width: 250,
+            },
+            {
+                field: "carName",
+                headerName: "Car",
+                valueGetter: (_, row) => row?.sessionData.car,
+                width: 250,
+            },
+            {
+                field: "driverName",
+                headerName: "Driver",
+                valueGetter: (_, row: LapData) => row?.sessionData.driver,
+                width: 200,
+            },
+            {
+                field: "lapTimestamp",
+                headerName: "Date",
+                valueFormatter: (value: Timestamp) => {
+                    return value.toDate().toLocaleString("en-fr", {
+                        dateStyle: "short",
+                    });
+                },
+                width: 160,
+                sortingOrder: ["asc", null],
+            },
+            {
+                field: "expiresAt",
+                headerName: "Expires",
+                width: 80,
+                valueGetter: (value) => !!value,
+                renderCell: ({ value }) =>
+                    value && (
+                        <Box
+                            display="flex"
+                            flexDirection="row"
+                            justifyContent="center"
+                            alignItems="center"
+                            height="stretch"
+                        >
+                            <TbClockX color={palette.warning.main} />
+                        </Box>
+                    ),
+            },
+        ],
+        [palette],
     );
 
     return (
