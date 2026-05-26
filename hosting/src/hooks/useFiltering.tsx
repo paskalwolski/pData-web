9238169b1d624ba8f8eeb262ba66ca6e746b228f
Paskal W <paskal.wolski@gmail.com>
2026-05-26 12:02:16 +0200

    [Filtering] Adds table filtering context provider

---
+++ hosting/src/hooks/useFiltering.tsx
@@ -0,0 +1,84 @@
+import { GridFilterItem } from "@mui/x-data-grid";
+import {
+    createContext,
+    useCallback,
+    useContext,
+    useMemo,
+    useState,
+} from "react";
+
+interface FilteringContext {
+    filterItems: GridFilterItem[];
+    addFilterItem: (filterItem: GridFilterItem) => void;
+    removeFilterItem: (filterId: string) => void;
+    clearFilterItems: () => void;
+    replaceFilterItem: (filterItem: GridFilterItem) => void;
+}
+
+const FilteringContext = createContext<FilteringContext>({
+    filterItems: [],
+    addFilterItem: () => undefined,
+    removeFilterItem: () => undefined,
+    replaceFilterItem: () => undefined,
+    clearFilterItems: () => undefined,
+});
+
+const FilteringProvider = ({ children }: { children: React.ReactNode }) => {
+    const [filterItems, setFilterItems] = useState<GridFilterItem[]>([]);
+
+    const addFilterItem = useCallback(
+        (filterItem: GridFilterItem) =>
+            setFilterItems((existingItems) => [
+                ...existingItems,
+                { id: crypto.randomUUID(), ...filterItem },
+            ]),
+        [],
+    );
+
+    const removeFilterItem = useCallback(
+        (filterId: string) =>
+            setFilterItems((existingItems) =>
+                existingItems.filter((f) => f.id === filterId),
+            ),
+        [],
+    );
+
+    const replaceFilterItem = useCallback(
+        (filterItem: GridFilterItem) =>
+            setFilterItems((existingItems) =>
+                existingItems.map((f) =>
+                    f.id === filterItem.id ? filterItem : f,
+                ),
+            ),
+        [],
+    );
+
+    const clearFilterItems = useCallback(() => setFilterItems([]), []);
+
+    const effectiveValue = useMemo(
+        () => ({
+            filterItems,
+            addFilterItem,
+            clearFilterItems,
+            removeFilterItem,
+            replaceFilterItem,
+        }),
+        [
+            addFilterItem,
+            filterItems,
+            removeFilterItem,
+            replaceFilterItem,
+            clearFilterItems,
+        ],
+    );
+
+    return (
+        <FilteringContext.Provider value={effectiveValue}>
+            {children}
+        </FilteringContext.Provider>
+    );
+};
+
+const useFiltering = () => useContext(FilteringContext);
+
+export { FilteringProvider, useFiltering };
