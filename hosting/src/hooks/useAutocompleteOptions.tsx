3116894cb4c0d2a146111bc682bf9ccc1d91a739
Paskal W <paskal.wolski@gmail.com>
2026-05-26 23:32:14 +0200

    [Filtering] Adds hook to fetch data for fitering columns using field identifier

---
+++ hosting/src/hooks/useAutocompleteOptions.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { AutocompleteOption } from "../components/TableFilter/types";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+
+// Allow 'let' so that we maintain this value until page refresh
+// eslint-disable-next-line prefer-const
+let cachedOptions: Record<
+    AutocompleteCollection,
+    AutocompleteOption[] | undefined
+> = { drivers: undefined };
+
+export type AutocompleteCollection = "drivers";
+
+const useAutocompleteOptions = (
+    field: AutocompleteCollection,
+): [AutocompleteOption[], boolean] => {
+    const [options, setOptions] = useState<AutocompleteOption[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        let cancelled = false;
+
+        if (!field) {
+            setOptions([]);
+            setLoading(false);
+            return;
+        }
+
+        async function fetchFilterOptions() {
+            if (cachedOptions[field]) {
+                setOptions(cachedOptions[field]);
+                setLoading(false);
+                return;
+            }
+
+            const snapshot = await getDocs(collection(db, field));
+            const optionValues = snapshot.docs.map((d) => ({
+                id: d.id,
+                label: d.id,
+            }));
+            cachedOptions[field] = optionValues;
+            if (!cancelled) {
+                setOptions(optionValues);
+                setLoading(false);
+            }
+        }
+
+        fetchFilterOptions();
+        return () => {
+            // Cleanup: Discard the result
+            cancelled = true;
+        };
+    }, [field]);
+
+    return [options, loading];
+};
+
+export { useAutocompleteOptions };
