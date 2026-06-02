281a402f36b8216e55201923dd4dcfaac860af86
Paskal W <paskal.wolski@gmail.com>
2026-06-02 09:34:41 +0200

    [DevX] Drops deprecated file

--- hosting/src/components/Track/utils.ts
+++
@@ -1,4 +0,0 @@
-export const bufferDomain = (domain) => [
-    domain[0] - Math.abs(0.01 * domain[0]), // Make sure negative values are reduced
-    domain[1] + Math.abs(domain[1] * 0.01),
-]; // Expand by ~10%
