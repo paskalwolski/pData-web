c34ec396c0537ec6d8d59dfdec0be5392cdf8bd9
Paskal W <paskal.wolski@gmail.com>
2026-06-13 11:31:09 +0200

    [Meta] Switches table autocompletes to use updated metadata structure

--- hosting/src/hooks/useAutocompleteOptions.tsx
+++ hosting/src/hooks/useAutocompleteOptions.tsx
@@ -1,60 +1,40 @@
 import { useEffect, useState } from "react";
 import { AutocompleteOption } from "../components/TableHeaderFilter";
-import {
-    collection,
-    getDocsFromCache,
-    getDocsFromServer,
-} from "firebase/firestore";
-import { db } from "../firebase";
+
 import { Entity } from "../types";
-import { useCollectionMetadataState } from "./useCollectionMeta";
+import { useCTDContext } from "../context/CTDContext/useCTDContext";
 
 const useAutocompleteOptions = (
     field: Entity,
 ): [AutocompleteOption[], boolean] => {
     const [options, setOptions] = useState<AutocompleteOption[]>([]);
-    const [loading, setLoading] = useState(true);
 
-    const [cacheState, isLoadingCacheState] = useCollectionMetadataState(field);
+    const { loading, ...ctdContext } = useCTDContext();
 
     useEffect(() => {
         let cancelled = false;
 
         if (!field) {
             setOptions([]);
-            setLoading(false);
             return;
         }
 
         async function fetchFilterOptions() {
-            if (isLoadingCacheState) {
+            if (loading || cancelled) {
                 return;
             }
-            const cacheValid = cacheState === "valid";
-            console.log(cacheValid);
-            const fieldDocGetter = cacheValid
-                ? getDocsFromCache(collection(db, field)).catch(() =>
-                      getDocsFromServer(collection(db, field)),
-                  )
-                : getDocsFromServer(collection(db, field));
-            const fieldDocs = await fieldDocGetter;
-            const optionValues = fieldDocs.docs.map((d) => ({
-                id: d.id,
-                label: d.data()?.name ?? d.id,
-            }));
-
-            if (!cancelled) {
-                setOptions(optionValues);
-                setLoading(false);
-            }
+            const metaName = ctdContext?.[field];
+            const autocompleteOptions = Object.entries(metaName.nameMap).map(
+                ([id, label]) => ({ id, label }),
+            );
+            setOptions(autocompleteOptions);
+            return () => {
+                // Cleanup: Discard the result
+                cancelled = true;
+            };
         }
-
         fetchFilterOptions();
-        return () => {
-            // Cleanup: Discard the result
-            cancelled = true;
-        };
-    }, [field, cacheState, isLoadingCacheState]);
+    }, [ctdContext, field, loading]);
 
     return [options, loading];
 };
